e27b99c6b2da9e105ce4b58d2c84cd925de3d963
wenzhe zhao <venturezhao@gmail.com>
2018-09-04 21:49:39 +0800

    Update sample.asm

--- sample.asm
+++ sample.asm
@@ -1,18 +1,31 @@
 # sample assemble
-define IMG_ADDR 0x0000
-define WT_ADDR 0x10000
+define IMG_ADDR   0x10000
+define IMG_L_LEN  0x00100
+define WT_ADDR    0x20000
 
-.text_sect
-    # load fm & weight data
-    addi r10, r0, IMG_ADDR
-    xload rx0, r10, 0
+.data_sect(0x10000)
+
+
+.text_sect(0x0000)
+    # load weight data
     addr r11, r0, WT_ADDR
-    xload ry0, r11, 0
+    xload ry0(0), r11
+    jnz csr.ld $
+    # load 1st line of featuremap
+    addi r10, r0, IMG_ADDR
+    xload rx0(1), r10
+    jnz csr.ld $
+    # load 2nd line of featuremap
+    addi r10, r10, LMG_L_LEN
+    xload rx0(1), r10
+    jnz csr.ld $
+    # feed padding
     # set offest
     addi r12, r0, 0
     addi r13, r0, 0
     # do conv operation
-    xmul rp0, rx0, ry0, r12, r13
+    xmul rp0, rx0(r12), ry0(r13)
     addi r12, r12, 1
     addi r13, r13, 1
-    xmac rp0, rx0, ry0, r12, r13, 8
+    xmac rp0, rx0(r12), ry0(r13), 8
+    # 
